310b68237e134a8e2ed420d1b6d7d637fe6d119e
George Wanjohi <gwanjohim@gmail.com>
2022-06-30 20:50:36 +0300

    Update UnitsOfMeasure.fsx

--- UnitsOfMeasure.fsx
+++ UnitsOfMeasure.fsx
@@ -1,4 +1,5 @@
-// distance - time - velocity
+(* distance - time - velocity *)
+
 [<Measure>]
 type m
 
@@ -9,7 +10,19 @@ type s
 type speed = m / s
 
 
-// fluids
+// Calculate speed from distance and time
+let computeSpeed distance time : decimal<speed> =
+    if (time = 0.0M<s>) then
+        failwith "The supplied time is 0 seconds. Speed not calculated!"
+    distance / time //switching the arguments results in compiler error
+
+
+// The order of arguments do matter here as always
+let x = computeSpeed 1.0M<m> 0.0m<s>
+
+
+(* fluids *)
+
 [<Measure>]
 type liter
 
@@ -17,8 +30,5 @@ type liter
 type gallon
 
 let convertGallon2Liter (x: decimal<liter>) = x / 3.785411784M<liter/gallon>
-
 let sixLiters = 6.0m<liter> //6.0m is not the same as 6.0<liter>
 let sixLitersInGallon = convertGallon2Liter sixLiters
-
-let speed (distance: decimal<m>) (time: decimal<s>) = distance / time
